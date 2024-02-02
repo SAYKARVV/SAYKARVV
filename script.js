cd1142a0dbc2dcdba299a84185e9312e80d489d8
vaibhav saykar <154413525+SAYKARVV@users.noreply.github.com>
2024-02-02 18:03:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSignUpModal() {
+    document.getElementById('signUpModal').style.display = 'flex';
+}
+
+function closeSignUpModal() {
+    document.getElementById('signUpModal').style.display = 'none';
+}
